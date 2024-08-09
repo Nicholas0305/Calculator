8ec0a0b0fcc7d5c51b8220ca0c5ea49a3195e4c0
Nicholas <najernigin@gmail.com>
2024-08-08 19:31:26 -0500

    NavBar init / File Cleanup

---
+++ calculator/src/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+function MainPage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>This is the main page</h1>
+    </div>
+  );
+}
+export default MainPage;
